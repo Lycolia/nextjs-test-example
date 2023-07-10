75d3dbc22f18027f9e53ff7255315a6c9209d2d1
Lycolia Rizzim <lycolia@lycolia.info>
2023-07-10 12:29:22 +0900

    update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,11 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/preset-scss',
-  ],
-  framework: '@storybook/react',
-  core: {
-    builder: 'webpack5',
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: true,
   },
 };

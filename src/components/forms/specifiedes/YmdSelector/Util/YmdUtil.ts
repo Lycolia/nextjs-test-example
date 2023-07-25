ab0355394f834bf0980782d5dfbd1b0c92ac87d4
Lycolia Rizzim <lycolia@lycolia.info>
2023-07-25 09:31:48 +0900

    基本文字列が来るので型変更

--- src/components/forms/specifiedes/YmdSelector/Util/YmdUtil.ts
+++ src/components/forms/specifiedes/YmdSelector/Util/YmdUtil.ts
@@ -4,11 +4,11 @@ export namespace YmdUtil {
   export const DefaultValue = '';
 
   export const createYearOptions = (
-    minYearValue?: number,
-    maxYearValue?: number
+    minYearValue?: string,
+    maxYearValue?: string
   ) => {
-    const maxYear = maxYearValue ?? new Date().getFullYear();
-    const minYear = minYearValue ?? 1914;
+    const maxYear = Number(maxYearValue ?? new Date().getFullYear());
+    const minYear = Number(minYearValue ?? 1914);
     const yearItems = new Array<SelectOption>();
     for (let year = maxYear; year >= minYear; year--) {
       const yearValue = year.toString();
@@ -39,8 +39,9 @@ export namespace YmdUtil {
     return dt.getDate();
   };
 
-  export const createDayOptions = (yearValue: number, monthValue: number) => {
-    const lastDayOfMonth = getLastDayOfMonth(yearValue, monthValue) + 1;
+  export const createDayOptions = (yearValue: string, monthValue: string) => {
+    const lastDayOfMonth =
+      getLastDayOfMonth(Number(yearValue), Number(monthValue)) + 1;
     const dayItems = new Array<SelectOption>();
     for (let day = 1; day <= lastDayOfMonth; day++) {
       const dayValue = day.toString();
